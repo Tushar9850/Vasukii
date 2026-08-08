ed02f0561c37c01d6281de5c0f15073c08a41e68
Tushar9850 <Kashyaptushar985@gmail.com>
2026-08-08 11:08:21 +0530

    Update image path for Areca Palm

--- src/data.js
+++ src/data.js
@@ -119,7 +119,7 @@ export const PRODUCTS = [
     name: "Areca Palm",
     price: "₹649",
     tint: "#4CA35A",
-    image: "/plants/Snake_plant.jpg"
+    image: "/plants/Areca Palm.png"
     blurb: "Big, breezy fronds that instantly make a corner feel alive.",
   },
   {
